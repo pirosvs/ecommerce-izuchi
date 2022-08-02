e7640fffe053dea6726a37956d197fd79add4f5f
darmstrong <armstrong2579@gmail.com>
2022-08-01 18:29:25 -0700

    add relationships to index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,37 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    //field referenced in the association must have a unique constraint placed on it. 
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  //call this whatever you link - related to how the data will be returned (which key)
+  as: ''
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: ''
+});
 
 module.exports = {
   Product,
