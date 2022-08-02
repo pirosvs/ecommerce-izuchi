4c5c80b736a723d9528755a70720879ce4bc4b46
darmstrong <armstrong2579@gmail.com>
2022-08-01 21:16:39 -0700

    display products with categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -18,7 +20,9 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk();
+    const categoryData = await Category.findByPk({
+      include: [{ model: Product }],
+    });
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
