0ea2bb9c8b0140740ed9ccb82e8bc5ac6c288c61
darmstrong <armstrong2579@gmail.com>
2022-08-01 21:42:05 -0700

    get by id routes now work, add id param to find method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk({
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
     if (!categoryData) {
