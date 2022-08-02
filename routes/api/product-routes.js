618b8c03f0160f1998a54e56c2b9ab757b72ef5b
darmstrong <armstrong2579@gmail.com>
2022-08-01 21:21:11 -0700

    get routes for product show category and tag info

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(400).json(err);
@@ -20,7 +22,9 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk();
+    const productData = await Product.findByPk({
+      include: [{ model: Category }, { model: Tag }],
+    });
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
       return;
